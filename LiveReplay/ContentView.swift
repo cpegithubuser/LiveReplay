10e2bf92af350f8f8f69af02b8a6acb0b55053ad
Albert Soong <albert.soong@gmail.com>
2026-01-22 15:35:54 -0800

    Initial Commit

---
+++ LiveReplay/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LiveReplay
+//
+//  Created by Albert Soong on 1/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LiveReplay/LiveReplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveReplayApp.swift
+//  LiveReplay
+//
+//  Created by Albert Soong on 1/22/26.
+//
+
+import SwiftUI
+
+@main
+struct LiveReplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
